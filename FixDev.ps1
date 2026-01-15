66c17dc8b912102953398b0815d2bf9996fb52eb
Project <v5eyiv5eyi7216072160@gmail.com>
2026-01-15 14:59:47 +0300

    Add complete project: GitHub workflows, gitignore, and source files

---
+++ FixDev.ps1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS
+    This Script is used as a target for the dev alias.
+.DESCRIPTION
+    This Script provides a simple way to start the bleeding edge release of FixOs.
+.EXAMPLE
+    irm "github.com/DeveIopmentSpace/FixOs/raw/Dev/FixOs.ps1" | iex
+    OR
+    Run in Admin Powershell >  ./FixDev.ps1
+#>
---
+++ FixOs.ps1
@@ -0,0 +1,7 @@
+<#
+- MORE INFO = https://github.com/DeveIopmentSpace/FixOs
+- NOTES
+    Version: 1.0.0
+    Author: Project/Development Space
+    Requires: Administrator privileges
+#>
