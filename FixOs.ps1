4949c2ef851453eb0a0a76db5bd10e832c434809
Project <v5eyiv5eyi7216072160@gmail.com>
2026-01-15 15:26:17 +0300

    dev branch

--- FixOs.ps1
+++ FixOs.ps1
@@ -1,7 +1,7 @@
 <#
 - MORE INFO = https://github.com/DeveIopmentSpace/FixOs
 - NOTES
-    Version: 1.0.0
+    Version: 1.0.1
     Author: Project/Development Space
     Requires: Administrator privileges
 #>
