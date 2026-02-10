98a7046494d3cc508474b7a77ef964b7c0a2ee98
Project <v5eyiv5eyi7216072160@gmail.com>
2026-02-10 07:38:38 +0300

    Update v1.8

--- src/FixOs.ps1
+++ src/FixOs.ps1
@@ -69,6 +69,28 @@ try {
     $Host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.Size(120, 9999)
 }
 
+function Create-ToolboxShortcut {
+    try {
+        $desktopPath = [Environment]::GetFolderPath("Desktop")
+        $shortcutPath = Join-Path $desktopPath "Toolbox.lnk"
+        
+        
+        $toolboxUrl = "https://raw.githubusercontent.com/DeveIopmentSpace/FixOs/dev/Toolbox/src/Toolbox.ps1"
+        
+        $WshShell = New-Object -ComObject WScript.Shell
+        $Shortcut = $WshShell.CreateShortcut($shortcutPath)
+        $Shortcut.TargetPath = "powershell.exe"
+        $Shortcut.WorkingDirectory = "$env:USERPROFILE"
+        $Shortcut.Description = "FixOs Toolbox - Run as Administrator"
+        $Shortcut.IconLocation = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe,0"
+        $Shortcut.Save()
+        
+        return $true
+    } catch {
+        return $false
+    }
+}
+
 function Show-Menu {
     Clear-Host
 
